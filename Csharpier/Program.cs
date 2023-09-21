600b78ddc438b8bae252aa71987c5355b81b05e5
Kuorusagin <pedro.souza27082@gmail.com>
2023-09-21 17:51:42 -0300

    Treinos em C#, program que adiciona produtos numa lista, e os lista, assim como também acrescenta quantidades, porém não as salva

---
+++ Csharpier/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Csharpier.models;
+using Newtonsoft.Json;
+//Lê Json
+// string arquivo = File.ReadAllText("Vendas.Json");
+// List<Vendajs> listaVenda = JsonConvert.DeserializeObject<List<Vendajs>>(arquivo);
+
+// foreach (Vendajs venda in listaVenda)
+// {
+//     Console.WriteLine($"ID: {venda.Id}, Produto: {venda.Produto}, Preco:{venda.Preco}, Data: {venda.Datavenda.ToString("dd/MM/yyyy")}");
+// }
+
+
+
+
+//cria Json 
+venda v1 = new venda();
+
+string opcao = string.Empty;
+bool exibirMenu = true;
+
+while (exibirMenu)
+{
+    Console.Clear();
+    Console.WriteLine("Digite o numero da opção desejada: \n");
+    Console.WriteLine("1 - Cadastrar SKU");
+    Console.WriteLine("2 - Exibir SKU's");
+    Console.WriteLine("3 - Adicionar quantidade de SKU's");
+    Console.WriteLine("4 - SAIR");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            v1.adicionarProduto();
+            break;
+        case "2":
+            v1.exibeProdutos();
+            break;
+        case "3":
+            v1.Venda();
+            break;
+        case "4":
+            exibirMenu = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+    Console.WriteLine("Pressione ENTER para continuar");
+    Console.ReadLine();
+}
+Console.WriteLine("Programa encerrado");
+
+
+
+
+// string serializado = JsonConvert.SerializeObject(Listavendas, Formatting.Indented);
+// File.WriteAllText("Vendas.Json", serializado);
+// Console.WriteLine(serializado);
