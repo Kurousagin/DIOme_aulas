4e6ee67a9eb358b33319c5c73986337fdfd0cd25
Pedro <pedro.souza27082@gmail.com>
2024-04-03 12:09:30 -0300

    add beecrowd exercicies

---
+++ Becrowd/bee.1006/bee1006.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var A,B,C float64
+	
+	fmt.Scan(&A)
+	fmt.Scan(&B)
+	fmt.Scan(&C)
+
+	
+	media := (A*2 + B*3 + C*5) / (2 + 3 + 5)
+	
+	fmt.Printf("MEDIA = %.1f\n", media)
+	
+
+}
---
+++ Becrowd/bee.1007/bee1007.go
@@ -0,0 +1,19 @@
+package main
+ 
+import (
+    "fmt"
+)
+ 
+func main() {
+
+  var A,B,C,D int64
+  
+  	fmt.Scan(&A)
+	fmt.Scan(&B)
+	fmt.Scan(&C)
+    fmt.Scan(&D)
+  
+  diferenca := (A * B - C * D)
+  
+  fmt.Printf("DIFERENCA = %d\n",diferenca)
+}
