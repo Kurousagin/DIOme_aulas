83810531f706a49e9da108a24ee9a29d3c86457f
Pedro <pedro.souza27082@gmail.com>
2024-04-22 10:25:10 -0300

    finish bootcamp and added calculator

--- Becrowd/bee.1006/bee1006.go
+++ Becrowd/bee.1006/bee1006.go
@@ -5,16 +5,17 @@ import (
 )
 
 func main() {
-	var A,B,C float64
-	
+	media(1,4,5)
+}
+
+func media(A float64, B float64, C float64) float64 {
+
 	fmt.Scan(&A)
 	fmt.Scan(&B)
 	fmt.Scan(&C)
 
-	
 	media := (A*2 + B*3 + C*5) / (2 + 3 + 5)
-	
-	fmt.Printf("MEDIA = %.1f\n", media)
-	
 
+	fmt.Printf("MEDIA = %.1f\n", media)
+	return media
 }
